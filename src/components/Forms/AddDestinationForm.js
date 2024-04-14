a4eabfa165f0b6f495806c274cb775cf463839c0
chakibamine <aminechakib2003@gmail.com>
2024-04-14 19:20:43 +0100

    DestinationPage

---
+++ src/components/Forms/AddDestinationForm.js
@@ -0,0 +1,135 @@
+import React, { useState } from 'react'
+import { Button, Checkbox, Form, Input, Upload, Radio, Modal, Rate, Select } from 'antd';
+import { PlusOutlined, AppstoreAddOutlined } from '@ant-design/icons';
+const onFinish = (values) => {
+    console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+};
+export const AddDestinationForm = () => {
+
+    const { TextArea } = Input;
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+    const normFile = (e) => {
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e?.fileList;
+    };
+    const [modal1Open, setModal1Open] = useState(false);
+    const options = [];
+    for (let i = 10; i < 36; i++) {
+        options.push({
+            value: i.toString(36) + i,
+            label: i.toString(36) + i,
+        });
+    }
+    const handleChange = (value) => {
+        console.log(`Selected: ${value}`);
+    };
+    const [size, setSize] = useState('middle');
+    return (
+        <>
+            <Button type='primary' style={{ float: 'right' }} onClick={() => setModal1Open(true)}><AppstoreAddOutlined />Add Destination</Button>
+            <Modal
+                title="Add Destination"
+                style={{ top: 20 }}
+                visible={modal1Open}
+                onOk={() => setModal1Open(false)}
+                onCancel={() => setModal1Open(false)}
+                footer={[
+                    <Button key="cancel" onClick={() => setModal1Open(false)}>
+                        Cancel
+                    </Button>,
+                    <Button key="submit" type="primary" form="userForm" htmlType="submit">
+                        Insert
+                    </Button>
+                ]}
+            >
+                <Form
+                    id="userForm"
+                    layout="vertical"
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="Name"
+                        name="Name"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input hotel Name!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Location"
+                        name="Location"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input hotel Location!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Description"
+                        name="Description"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input hotel Description!',
+                            },
+                        ]}
+                    >
+                        <TextArea rows={4} />
+                    </Form.Item>
+                    <Form.Item
+                        label="Rating"
+                        name="Rating"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please Rate Tihs hotel!',
+                            },
+                        ]}
+                    >
+                        <Rate allowHalf defaultValue={1} />
+                    </Form.Item>
+                    <Form.Item label="Image" valuePropName="fileList" getValueFromEvent={normFile}>
+                        <Upload action="/upload.do" listType="picture-card">
+                            <button
+                                style={{
+                                    border: 0,
+                                    background: 'none',
+                                }}
+                                type="button"
+                            >
+                                <PlusOutlined />
+                                <div
+                                    style={{
+                                        marginTop: 8,
+                                    }}
+                                >
+                                    Upload
+                                </div>
+                            </button>
+                        </Upload>
+                    </Form.Item>
+                </Form>
+
+            </Modal>
+        </>
+    )
+}
--- src/pages/destination/Destinations.js
+++ src/pages/destination/Destinations.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-import { Divider, Table, Checkbox, Button, Input,Rate } from 'antd';
-import { Link } from 'react-router-dom';
+import { Table, Checkbox, Button, Input,Rate } from 'antd';
 import { MenuUnfoldOutlined } from '@ant-design/icons';
 import './Destinations.css'
-import { AddHotelForm } from 'components/Forms/AddHotelForm';
+import { AddDestinationForm } from 'components/Forms/AddDestinationForm';
 
 const columns = [
     {
@@ -21,11 +20,6 @@ const columns = [
         dataIndex: 'Description',
         key: '3',
         render: (text) => <span>{text.slice(0, 25)}{text.length > 25 ? "..." : ""}</span>,
-    },
-    {
-        title: 'Destination',
-        dataIndex: 'Destination',
-        key: '4',
     },
     {
         title: 'Rating',
@@ -46,70 +40,60 @@ const data = [{
     "Name": "Reinger and Sons",
     "Location": "Room 1511",
     "Description": "Athscl unsp type bypass of left leg w ulcer oth prt low leg",
-    "Destination": "Taverny",
     "Rating": 4.5
   }, {
     "id": 2,
     "Name": "Pouros, Kessler and Metz",
     "Location": "Suite 65",
     "Description": "Other non-in-line roller-skating accident, initial encounter",
-    "Destination": "Banjar Bau Kawan",
     "Rating": 4.0
   }, {
     "id": 3,
     "Name": "O'Connell Group",
     "Location": "Room 1292",
     "Description": "Prsn outsd pk-up/van inj in nonclsn trnsp acc nontraf, subs",
-    "Destination": "Blois",
     "Rating": 4.3
   }, {
     "id": 4,
     "Name": "Batz Group",
     "Location": "Suite 80",
     "Description": "Underdosing of unsp agents aff the GI sys, init",
-    "Destination": "Watuagung",
     "Rating": 3.4
   }, {
     "id": 5,
     "Name": "Jacobi LLC",
     "Location": "Suite 1",
     "Description": "Toxic effect of venom of black widow spider, acc, init",
-    "Destination": "Dulangan",
     "Rating": 1.7
   }, {
     "id": 6,
     "Name": "Howell-Murray",
     "Location": "Apt 2",
     "Description": "Other fractures of lower leg",
-    "Destination": "San Antonio",
     "Rating": 1.4
   }, {
     "id": 7,
     "Name": "Swaniawski LLC",
     "Location": "Apt 511",
-    "Description": "Major laceration of celiac artery, sequela",
-    "Destination": "Louveira",
+    "Description": "Major laceration of celiac artery, sequela",  
     "Rating": 2.5
   }, {
     "id": 8,
     "Name": "Rolfson-Dickinson",
     "Location": "Apt 335",
     "Description": "Strain of unsp quadriceps muscle, fascia and tendon, sequela",
-    "Destination": "Kybartai",
     "Rating": 2.1
   }, {
     "id": 9,
     "Name": "Stokes-Ritchie",
     "Location": "Apt 775",
     "Description": "Sprain of carpal (joint)",
-    "Destination": "Qilin",
     "Rating": 4.1
   }, {
     "id": 10,
     "Name": "Fritsch Inc",
     "Location": "16th Floor",
     "Description": "Pressure ulcer of left upper back, stage 3",
-    "Destination": "Wanjiazhuang",
     "Rating": 1.6
   },]
   
@@ -153,7 +137,7 @@ export const Destinations = () => {
                 className={`checkbox-group ${filterList ? 'checkbox-group-active' : ''}`}
                 />
             )}
-            <AddHotelForm />
+            <AddDestinationForm />
             <Input style={{ float: 'right', width:"150px", marginRight:"10px"}} placeholder='Search in Table' onChange={(e)=> setSearch(e.target.value)} />
 
             <Table
