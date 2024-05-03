08bc6f21800f6e665ceb3f759cd3be7184982631
chakibamine <aminechakib2003@gmail.com>
2024-05-03 16:40:05 +0100

    changes

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Layout,
   Button,
@@ -7,108 +7,117 @@ import {
   Input,
   Col,
   Row,
-  Switch,
+  message,
 } from "antd";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/images/logo.png";
-// import { AntdFooter } from "components/Layout/Footer";
-// import { AntdHeader } from "components/Layout/Header";
-import { Header, Footer } from "components/Layout";
+import { client } from "utils/axios";
 
 const { Content } = Layout;
 const { Title } = Typography;
 
-// console.log(Menu);
-export default class SignIn extends Component {
-  render() {
-    const onFinish = (values) => {
-      console.log("values : ", values);
-    };
-    const onFinishFailed = (errInfo) => {
-      console.log("Error : ", errInfo);
-    };
-    function onChange(checked) {
-      console.log(`switch to ${checked}`);
+const SignIn = () => {
+  const navigate = useNavigate();
+
+  const onFinish = async (values) => {
+    try {
+      const response = await client.post("login/", values);
+      console.log("Login Successful: ", response.data);
+      // Redirect user to dashboard or wherever you want
+      navigate("/");
+    } catch (error) {
+      if (error.response.status === 400) {
+        message.error("Invalid email or password");
+      } else {
+        message.error("An error occurred. Please try again later");
+      }
     }
-    return (
-      <>
-        <div className="layout-default layout-signin">
+  };
 
-          <Content className="signin">
-            <Row gutter={[24, 0]} justify="space-around">
-              <Col
-                xs={{ span: 24, offset: 0 }}
-                lg={{ span: 6, offset: 2 }}
-                md={{ span: 12 }}
+  const onFinishFailed = (errInfo) => {
+    console.log("Error : ", errInfo);
+    message.error("Please complete all the fields");
+  };
+
+  return (
+    <>
+      <div className="layout-default layout-signin">
+        <Content className="signin">
+          <Row gutter={[24, 0]} justify="space-around">
+            <Col
+              xs={{ span: 24, offset: 0 }}
+              lg={{ span: 6, offset: 2 }}
+              md={{ span: 12 }}
+            >
+              <Title className="mb-15" style={{ fontFamily: 'Circular, sans-serif' }}>Sign In</Title>
+              <Title className="text-muted"  style={{ fontFamily: 'Circular, sans-serif' }} level={5}>
+                Enter your email and password to sign in
+              </Title>
+              <Form
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                layout="vertical"
+                className="row-col"
               >
-                <Title className="mb-15" style={{ fontFamily: 'Circular, sans-serif' }}>Sign In</Title>
-                <Title className="text-muted"  style={{ fontFamily: 'Circular, sans-serif' }} level={5}>
-                  Enter your email and password to sign in
-                </Title>
-                <Form
-                  onFinish={onFinish}
-                  onFinishFailed={onFinishFailed}
-                  layout="vertical"
-                  className="row-col"
+                <Form.Item
+                  className="username"
+                  label="Email"
+                  name="email"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your email!",
+                    },
+                  ]}
                 >
-                  <Form.Item
-                    className="username"
-                    label="Email"
-                    name="email"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your email!",
-                      },
-                    ]}
-                  >
-                    <Input placeholder="Email" />
-                  </Form.Item>
+                  <Input placeholder="Email" />
+                </Form.Item>
 
-                  <Form.Item
-                    className="username"
-                    label="Password"
-                    name="password"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your password!",
-                      },
-                    ]}
+                <Form.Item
+                  className="username"
+                  label="Password"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your password!",
+                    },
+                  ]}
+                >
+                  <Input.Password placeholder="Password" />
+                </Form.Item>
+                <Form.Item>
+                  <Button
+                    htmlType="submit"
+                    style={{ width: "100%", backgroundColor:"#26baeb",color:"white" }}
                   >
-                    <Input placeholder="Password" />
-                  </Form.Item>
-                  <Form.Item>
-                    <Button
-                      htmlType="submit"
-                      style={{ width: "100%", backgroundColor:"#26baeb",color:"white" }}
-                    >
-                      SIGN IN
-                    </Button>
-                  </Form.Item>
-                  <p className="font-semibold text-muted">
-                    Don't have an account?{" "}
-                    <Link to="/sign-up" className="text-dark font-bold">
-                      Sign Up
-                    </Link>
-                  </p>
-                </Form>
-              </Col>
-              <Col
-                className="sign-img"
-                style={{ padding: 12 }}
-                xs={{ span: 24 }}
-                lg={{ span: 12 }}
-                md={{ span: 12 }}
-              >
-                <img src={logo} alt="" style={{ width: "60%", marginLeft: "100px", marginTop: "50px" }} />
-              </Col>
-            </Row>
-          </Content>
-          {/* Footer */}
-          {/* <Footer /> */}
-        </div>
-      </>
-    );
-  }
+                    SIGN IN
+                  </Button>
+                </Form.Item>
+                <p className="font-semibold text-muted">
+                  Don't have an account?{" "}
+                  <Link to="/sign-up" className="text-dark font-bold">
+                    Sign Up
+                  </Link>
+                </p>
+              </Form>
+            </Col>
+            <Col
+              className="sign-img"
+              style={{ padding: 12 }}
+              xs={{ span: 24 }}
+              lg={{ span: 12 }}
+              md={{ span: 12 }}
+            >
+              <img src={logo} alt="" style={{ width: "60%", marginLeft: "100px", marginTop: "50px" }} />
+            </Col>
+          </Row>
+        </Content>
+        {/* Footer */}
+        {/* <Footer /> */}
+      </div>
+    </>
+  );
 }
+
+export default SignIn;
